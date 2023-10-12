72d065ee61c004f64b5d92edf61ed94f26090b92
Victor-Egas <victor.egas.fieet@gmail.com>
2023-10-11 23:04:57 -0500

    feat : first commit

---
+++ src/main/java/com/atipax/group/AtipaxGroupSystemApplication.java
@@ -0,0 +1,13 @@
+package com.atipax.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtipaxGroupSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtipaxGroupSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atipax/group/AtipaxGroupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipax.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtipaxGroupSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
